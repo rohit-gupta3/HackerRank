619947a2cdde4e0fc519ef382b8bb89368767167
ROHIT-KUMAR-GUPTA <82712515+rohit-gupta3@users.noreply.github.com>
2022-06-05 20:19:38 +0530

    Add files via upload
    Kangaroos reaching same Position.

---
+++ Problem Solving/NumberLineJumps.cpp
@@ -0,0 +1,95 @@
+/*
+You are choreographing a circus show with various animals. For one act, you are given two kangaroos
+ on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+
+The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+You have to figure out a way to get both kangaroos at the same location at the same time as part of the show.
+If it is possible, return YES, otherwise return NO.
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+    int res1=0,res2=0;
+    for(int i =0;i<=10000;i++)
+    {
+        res1 = x1 + (i*v1);
+        res2 = x2 + (i*v2);
+        if(res1==res2)
+        {
+            return "YES";
+        }
+    }
+    return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int x1 = stoi(first_multiple_input[0]);
+
+    int v1 = stoi(first_multiple_input[1]);
+
+    int x2 = stoi(first_multiple_input[2]);
+
+    int v2 = stoi(first_multiple_input[3]);
+
+    string result = kangaroo(x1, v1, x2, v2);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
